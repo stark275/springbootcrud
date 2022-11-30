f8608b8c4387668912d62923964b50e2f595670b
stark <stark1_5@live.com>
2022-12-01 00:26:13 +0100

    Read

--- src/main/java/com/spring/crud/app/product/controllers/ProductController.java
+++ src/main/java/com/spring/crud/app/product/controllers/ProductController.java
@@ -2,11 +2,14 @@ package com.spring.crud.app.product.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.crud.app.product.data.entities.Product;
 import com.spring.crud.app.product.data.repositositories.ProductRepository;
 
+@RestController
 public class ProductController {
     
     private ProductRepository prodRepo;
@@ -17,6 +20,7 @@ public class ProductController {
 
     @RequestMapping("products")
 
+    @GetMapping("/")
     public List<Product> index() {
         return  prodRepo.findAll();
     }
